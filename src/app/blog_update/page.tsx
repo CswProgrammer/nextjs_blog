32d748cd57cb253ee1b613be2ad136d5941f36c5
chenshuwei <754514987@qq.com>
2025-10-30 14:48:15 +0800

    fix: 用户初次使用，创建一个默认文档

--- src/app/blog_update/page.tsx
+++ src/app/blog_update/page.tsx
@@ -3,18 +3,37 @@ import { redirect } from "next/navigation";
 import { getUserInfo } from "@/lib/session";
 
 export default async function Work() {
-  let pathname = "/blog_update/0";
-
   const user = await getUserInfo();
-  if (user && user.id) {
-    const firstDoc = await db.docBlog.findFirst({
-      where: { userId: user.id, isDeleted: false || null },
-      orderBy: {
-        updatedAt: "desc",
-      },
-    });
-    if (firstDoc != null) pathname = `/blog_update/${firstDoc?.id}`; // 找到第一篇文档，然后跳转过去
+  if (user == null || !user.id) {
+    redirect("/");
+    return;
   }
 
-  redirect(pathname);
+  const firstDoc = await db.docBlog.findFirst({
+    where: { userId: user.id, isDeleted: false || null },
+    orderBy: {
+      updatedAt: "desc",
+    },
+  });
+  if (firstDoc != null) {
+    // 找到第一篇文档，然后跳转过去
+    redirect(`/blog_update/${firstDoc?.id}`);
+    return;
+  }
+
+  // 找不到文档，新建文档
+  const newDoc = await db.docBlog.create({
+    data: {
+      userId: user.id,
+      title: "AI blog 是什么",
+      content:
+        '{"type":"doc","content":[{"type":"paragraph","attrs":{"textAlign":"left"},"content":[{"type":"text","marks":[{"type":"bold"}],"text":"AI blog"},{"type":"text","text":" 是一个 AIGC 知识库平台，像国内的"},{"type":"text","marks":[{"type":"link","attrs":{"href":"https://docs.qq.com/","target":"_blank","rel":"noopener noreferrer nofollow","class":null}}],"text":"腾讯文档"},{"type":"text","text":"、"},{"type":"text","marks":[{"type":"link","attrs":{"href":"https://www.yuque.com/","target":"_blank","rel":"noopener noreferrer nofollow","class":null}}],"text":"语雀"},{"type":"text","text":"，国外的 "},{"type":"text","marks":[{"type":"link","attrs":{"href":"https://www.notion.so/","target":"_blank","rel":"noopener noreferrer nofollow","class":null}}],"text":"notion"},{"type":"text","text":" 。"}]},{"type":"paragraph","attrs":{"textAlign":"left"},"content":[{"type":"text","text":"你可以在这里创建文档，管理文档，编辑文档。可使用 "},{"type":"text","marks":[{"type":"highlight","attrs":{"color":"#ffc078"}}],"text":"AI 智能写作"},{"type":"text","text":"，快速生成、处理文字。"}]},{"type":"bulletList","content":[{"type":"listItem","content":[{"type":"paragraph","attrs":{"textAlign":"left"},"content":[{"type":"text","text":"使用页面下方的 AI 输入框，输入指令生成文字"}]}]},{"type":"listItem","content":[{"type":"paragraph","attrs":{"textAlign":"left"},"content":[{"type":"text","text":"选中一段文字，点击弹出菜单“Ask AI”，输入指令让 AI 处理文字"}]}]}]},{"type":"paragraph","attrs":{"textAlign":"left"}},{"type":"paragraph","attrs":{"textAlign":"left"},"content":[{"type":"text","text":"PS：你也可以"},{"type":"text","marks":[{"type":"link","attrs":{"href":"https://huashuiai.com/join","target":"_blank","rel":"noopener noreferrer nofollow","class":null}}],"text":"学习/参与"},{"type":"text","text":"这个项目的研发，提高个人能力，准备简历/面试项目。"}]},{"type":"paragraph","attrs":{"textAlign":"left"}}]}',
+      category: "默认分类", // 🔧 修复：必填字段
+    },
+  });
+  // 跳转
+  redirect(`/blog_update/${newDoc.id}`);
+
+  // 不渲染页面
+  return null;
 }
